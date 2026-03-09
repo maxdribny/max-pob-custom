79c9b48bb63ff61cc8767a57724807f21bf3e08a
LocalIdentity <localidentity2@gmail.com>
2026-03-10 06:22:30 +1100

    Fix Divine Blessing appearing in gem list
    The skill is no longer on a gem and only available through March of the Legion

--- src/Data/Skills/other.lua
+++ src/Data/Skills/other.lua
@@ -1175,8 +1175,8 @@ skills["GemDetonateMines"] = {
 	},
 }
 skills["SupportDivineBlessing"] = {
-	name = "Divine Blessing",
-	description = "Supports aura skills that create permanent auras around you, changing their reservation to a cost and making them temporary. Cannot support banner or stance skills.",
+	name = "SupportDivineBlessing",
+	hidden = true,
 	color = 1,
 	support = true,
 	requireSkillTypes = { SkillType.CanHaveBlessing, },
